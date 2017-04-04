f84c00b58f751320c5b877325bdb46e6004008f4
Matthew Adendorff <mattadendorff@gmail.com>
2017-04-04 15:14:32 +0200

    V1

---
+++ multiapp/helpers.py
@@ -0,0 +1,55 @@
+import requests
+import datetime
+
+URL = "http://mapit.code4sa.org/address?address=%s&generation=2&type=WD"
+URLxy = "http://mapit.code4sa.org/point/4326/%s,%s"
+
+
+def coords_to_ward(lon, lat):
+    url = URLxy % (lon, lat)
+    r = requests.get(url)
+    js = r.json()
+    ward_no = None
+    ward_id = None
+
+    for key in js:
+        if js[key]["type_name"] == 'Ward':
+            ward_no = js[key]["name"]
+            ward_id = js[key]["codes"]["MDB"]
+
+    return {
+        "ward_id": ward_id,
+        "ward": ward_no,
+        "address": 'Used geolocation data for Latitude: %.4f and Longitude: %.4f' % (float(lat), float(lon))
+    }
+
+
+def address_to_ward(address):
+    r = requests.get(URL % address)
+    js = r.json()
+    ward_no = None
+    ward_id = None
+    formatted_address = "Unknown"
+
+    for key in js:
+        if "type_name" in js[key]:
+            ward_no = js[key]["name"]
+            ward_id = js[key]["codes"]["MDB"]
+            ward_key = key
+
+    for address in js["addresses"]:
+        if ward_key in address["areas"]:
+            formatted_address = address["formatted_address"]
+
+    return {
+        "ward_id": ward_id,
+        "ward": ward_no,
+        "address": formatted_address
+    }
+
+
+def get_councillor(ward):
+    id = ward['ward_id']
+    r = requests.get('https://nearby.code4sa.org/councillor/ward-%s.json' % id)
+    js = r.json()
+    return js
--- multiapp/routes.py
+++ multiapp/routes.py
@@ -1,6 +1,7 @@
 from .models.models import *
 from multiapp.app import app
 from flask import render_template, request
+from helpers import *
 import urllib2
 import json
 # from .models.articles import SearchForm
@@ -11,7 +12,7 @@ import re
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form =SearchForm()
+    form = SearchForm()
 
     # url = 'http://data.opendata.durban/api/action/'
     # req = urllib2.Request(url)
@@ -22,7 +23,33 @@ def home():
     if request.method == 'POST':
 
         return render_template('home/home.html', form=form)
+
     else:
         return render_template('home/home.html', title='App 1', form=form)
 
 
+@app.route('/ward_councillor')
+def ward_councillor():
+    address = request.args.get("address")
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
+    ward = None
+
+    variables = {'missing': False}
+    candidates = []
+    if address:
+        ward = address_to_ward(address)
+    elif lat:
+        ward = coords_to_ward(lon, lat)
+
+    if ward:
+        if ward['ward']:
+            variables.update(ward)
+            councillor = get_councillor(ward)
+            variables["councillor"] = councillor
+            print councillor
+
+        else:
+            variables['missing'] = True
+
+    return render_template('councillor/councillor.html', **variables)
